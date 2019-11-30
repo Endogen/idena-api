3a3dfe5bfddbacd727602a0307b23d306ca92036
Endogen <081081@gmx.net>
2019-11-30 01:39:44 +0100

    Initial version of IDENA API

---
+++ api-test.py
@@ -0,0 +1,31 @@
+from api import IdenaAPI
+
+
+api = IdenaAPI()
+
+result = api.balance("0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74")
+print("Balance:", result["result"]["balance"])
+
+result = api.transactions("0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74", 10)
+print("Transactions:", result["result"]["transactions"])
+
+result = api.pending_transactions("0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74", 10)
+print("Pending Transactions:", result["result"]["transactions"])
+
+result = api.kill_identity("0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74")
+print("Identity Killed:", result["result"])
+
+result = api.go_online()
+print("Go Online:", result["result"])
+
+result = api.go_offline()
+print("Go Offline:", result["result"])
+
+result = api.send("0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74", "0x18a5eb84dc215a2f170ff1f78dc1873ed8d04d74", 1)
+print("Send:", result["result"])
+
+result = api.sync_status()
+print("Sync Status:", result["result"])
+
+result = api.node_version()
+print("Node Version:", result["result"])
--- api.py
+++ api.py
@@ -1,29 +1,4 @@
-import json
 import requests
-
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
-
-def list_args_to_comma_separated(func):
-    """Return function that converts list input arguments to comma-separated strings"""
-    def input_args(*args, **kwargs):
-        for v in kwargs:
-            # check in **kwargs for lists and convert to comma-separated string
-            if isinstance(kwargs[v], list): kwargs[v] = ','.join(kwargs[v])
-        # check in *args for lists and convert to comma-separated string
-        args=[','.join(v) if isinstance(v, list) else v for v in args]
-        return func(*args, **kwargs)
-    return input_args
-
-
-def get_comma_separated_values(values):
-    """Return the values as a comma-separated string"""
-
-    # Make sure values is a list or tuple
-    if not isinstance(values, list) and not isinstance(values, tuple):
-        values = [values]
-    return ','.join(values)
 
 
 class IdenaAPI:
@@ -32,166 +7,80 @@ class IdenaAPI:
     __API_PORT = "9009"
 
     def __init__(self, api_url=__API_URL, api_port=__API_PORT):
-        self.api_url = api_url
-        self.api_port = api_port
+        self.url = f"{api_url}:{api_port}"
 
-        self.request_timeout = 120
+    def balance(self, address):
+        payload = {
+            "method": "dna_getBalance",
+            "params": [f"{address}"],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-        self.session = requests.Session()
-        retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[502, 503, 504])
-        self.session.mount('http://', HTTPAdapter(max_retries=retries))
+    def transactions(self, address, count):
+        payload = {
+            "method": "bcn_transactions",
+            "params": [{"address": f"{address}", "countd": int(count)}],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-    def __request(self, url):
-        try:
-            response = self.session.get(url, timeout = self.request_timeout)
-            response.raise_for_status()
-            content = json.loads(response.content.decode('utf-8'))
-            return content
-        except Exception as e:
-            # check if json (with error message) is returned
-            try:
-                content = json.loads(response.content.decode('utf-8'))
-                raise ValueError(content)
-            # if no json
-            except json.decoder.JSONDecodeError:
-                pass
-            #except UnboundLocalError as e:
-            #    pass
-            raise
+    def pending_transactions(self, address, count):
+        payload = {
+            "method": "bcn_pendingTransactions",
+            "params": [{"address": f"{address}", "count": int(count)}],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-    def __api_url_params(self, api_url, params):
-        if params:
-            api_url += '?'
-            for key, value in params.items():
-                api_url += "{0}={1}&".format(key, value)
-            api_url = api_url[:-1]
-        return api_url
+    def kill_identity(self, address):
+        payload = {
+            "method": "dna_sendTransaction",
+            "params": [{"type": 3, "from": f"{address}", "to": f"{address}"}],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
+    def go_online(self):
+        payload = {
+            "method": "dna_becomeOnline",
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-    #---------- SIMPLE ----------#
-    @list_args_to_comma_separated
-    def get_price(self, ids, vs_currencies, **kwargs):
-        """Get the current price of any cryptocurrencies in any other supported currencies that you need"""
+    def go_offline(self):
+        payload = {
+            "method": "dna_becomeOffline",
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-        ids=ids.replace(' ','')
-        kwargs['ids'] = ids
-        vs_currencies=vs_currencies.replace(' ','')
-        kwargs['vs_currencies'] = vs_currencies
+    def send(self, from_address, to_address, amount):
+        payload = {
+            "method": "dna_sendTransaction",
+            "params": [{"from": from_address, "to": to_address, "amount": amount}],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-        api_url = '{0}simple/price'.format(self.api_url)
-        api_url = self.__api_url_params(api_url, kwargs)
+    def sync_status(self):
+        payload = {
+            "method": "bcn_syncing",
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-        return self.__request(api_url)
+    def node_version(self):
+        payload = {
+            "method": "dna_version",
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
 
-
-    @list_args_to_comma_separated
-    def get_token_price(self, id, contract_addresses, vs_currencies, **kwargs):
-        """Get the current price of any tokens on this coin (ETH only at this stage as per api docs) in any other supported currencies that you need"""
-
-        contract_addresses=contract_addresses.replace(' ','')
-        kwargs['contract_addresses'] = contract_addresses
-        vs_currencies=vs_currencies.replace(' ','')
-        kwargs['vs_currencies'] = vs_currencies
-
-        api_url = '{0}simple/token_price/{1}'.format(self.api_url, id)
-        api_url = self.__api_url_params(api_url, kwargs)
-        return self.__request(api_url)
-
-
-    def get_supported_vs_currencies(self):
-        """Get list of supported_vs_currencies"""
-
-        api_url = '{0}simple/supported_vs_currencies'.format(self.api_url)
-        return self.__request(api_url)
-
-
-    #---------- COINS ----------#
-    @list_args_to_comma_separated
-    def get_coins(self, **kwargs):
-        """List all coins with data (name, price, market, developer, community, etc)"""
-
-        api_url = '{0}coins'.format(self.api_url)
-        #['order', 'per_page', 'page', 'localization']
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
-
-
-    def get_coins_list(self):
-        """List all supported coins id, name and symbol (no pagination required)"""
-
-        api_url = '{0}coins/list'.format(self.api_url)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coins_markets(self, vs_currency, **kwargs):
-        """List all supported coins price, market cap, volume, and market related data (no pagination required)"""
-
-        kwargs['vs_currency'] = vs_currency
-
-        api_url = '{0}coins/markets'.format(self.api_url)
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_by_id(self, id, **kwargs):
-        """Get current data (name, price, market, ... including exchange tickers) for a coin"""
-
-        api_url = '{0}coins/{1}/'.format(self.api_url, id)
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_ticker_by_id(self, id, **kwargs):
-        """Get coin tickers (paginated to 100 items)"""
-
-        api_url = '{0}coins/{1}/tickers'.format(self.api_url, id)
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_history_by_id(self, id, date, **kwargs):
-        """Get historical data (name, price, market, stats) at a given date for a coin"""
-
-        kwargs['date'] = date
-
-        api_url = '{0}coins/{1}/history'.format(self.api_url, id)
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_market_chart_by_id(self, id, vs_currency, days):
-        """Get historical market data include price, market cap, and 24h volume (granularity auto)"""
-
-        api_url = '{0}coins/{1}/market_chart?vs_currency={2}&days={3}'.format(self.api_url, id, vs_currency, days)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_market_chart_range_by_id(self, id, vs_currency, from_timestamp, to_timestamp):
-        """Get historical market data include price, market cap, and 24h volume within a range of timestamp (granularity auto)"""
-
-        api_url = '{0}coins/{1}/market_chart/range?vs_currency={2}&from={3}&to={4}'.format(self.api_url, id, vs_currency, from_timestamp, to_timestamp)
-
-        return self.__request(api_url)
-
-
-    @list_args_to_comma_separated
-    def get_coin_status_updates_by_id(self, id, **kwargs):
-        """Get status updates for a given coin"""
-
-        api_url = '{0}coins/{1}/status_updates'.format(self.api_url, id)
-        api_url = self.__api_url_params(api_url, kwargs)
-
-        return self.__request(api_url)
+    def import_key(self, key, password):
+        payload = {
+            "method": "dna_importKey",
+            "params": [{"key": key, "password": password}],
+            "id": 1
+        }
+        return requests.post(self.url, json=payload).json()
